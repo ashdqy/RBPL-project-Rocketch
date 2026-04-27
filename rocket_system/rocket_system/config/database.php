32937d956ef6a3c96525ec76195c7d0ca881f544
Chusna Ashidiqi <124240043@student.upnyk.ac.id>
2026-04-28 02:04:13 +0700

    Update database.php

--- rocket_system/rocket_system/config/database.php
+++ rocket_system/rocket_system/config/database.php
@@ -1,5 +1,5 @@
 <?php
-$host     = 'sql390.infinityfree.com';
+$host     = 'sql309.infinityfree.com';
 $dbname   = 'if0_41770489_rocket2';
 $user     = 'if0_41770489';
 $password = 'rocketchsystem';
